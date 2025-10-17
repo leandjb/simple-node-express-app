8e286f13da7084ca0a136ede99de7a67b9697c18
Leandro <leandjb@gmail.com>
2025-10-16 22:13:02 -0500

    helmet security added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const helmet = require("helmet")
 
+const app = express();
 const PORT = 3000;
 
+app.use(helmet());
 app.get("/", (req, res) => {
     res.send("EXPRESS API Server Running...")
 });
